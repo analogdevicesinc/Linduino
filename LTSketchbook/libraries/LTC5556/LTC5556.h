71ed1e412f931e4a353636f7e9c6ecd1ea49f6bf
wsapia-adi <weston.sapia@analog.com>
2018-04-06 07:33:35 -0600

    - Update webpage and copyright info - Remove revision history - Delete @TBD statements

--- LTSketchbook/libraries/LTC5556/LTC5556.h
+++ LTSketchbook/libraries/LTC5556/LTC5556.h
@@ -21,46 +21,40 @@ IF : IF Attenuation Control Bits
 
 @endverbatim
 
-http://www.linear.com/product/LTC5556
+http://www.analog.com/en/products/rf-microwave/mixers/single-double-triple-balanced-mixers/ltc5556.html
 
-http://www.linear.com/product/LTC5556#demoboards
+Copyright 2018(c) Analog Devices, Inc.
 
-REVISION HISTORY
-$Revision: 4634 $
-$Date: 2016-01-28 11:51:17 -0700 (Thu, 28 Jan 2016) $
-
-Copyright (c) 2013, Linear Technology Corp.(LTC)
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in
+   the documentation and/or other materials provided with the
+   distribution.
+ - Neither the name of Analog Devices, Inc. nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+ - The use of this software may or may not infringe the patent rights
+   of one or more patent holders.  This license does not release you
+   from the requirement that you obtain separate licenses from these
+   patent holders to use this software.
+ - Use of the software either in source or binary form, must be run
+   on or directly connected to an Analog Devices Inc. component.
 
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those
-of the authors and should not be interpreted as representing official policies,
-either expressed or implied, of Linear Technology Corp.
-
-The Linear Technology Linduino is not affiliated with the official Arduino team.
-However, the Linduino is only possible because of the Arduino team's commitment
-to the open-source community.  Please, visit http://www.arduino.cc and
-http://store.arduino.cc , and consider a purchase that will help fund their
-ongoing work.
+THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 /*! @file
@@ -94,30 +88,21 @@ void LTC5556_write(uint8_t cs,                  //! Chip Select pin
 				   );
 
 //! Function to duplicate settings for both LTC5556 channels
-//! @return @tbd
 uint8_t LTC5556_dupl_settings();
 
 //! Function to apply unique settings for each LTC5556 channel
-//! @return @tbd
 uint16_t LTC5556_diff_settings();
 				   
 //! Take inputs and apply the settings to the mixer
-//! @return @tbd				   
 uint8_t LTC5556_settings();
 
 //! Function to get data from user for power mode
-//! @return @tbd
-uint8_t LTC5556_get_power_mode(char *prompt   //! @tbd
-                              );
+uint8_t LTC5556_get_power_mode(char *prompt);
 
 //! Get attenuation value from user
-//! @return @tbd
-uint8_t LTC5556_get_att(char *prompt			//! @tbd
-					    );          
+uint8_t LTC5556_get_att(char *prompt);
 
 //! Decode the register value read from the LTC5556
-//! @return void						
-void LTC5556_decode_output(uint8_t output		//! @tbd
-						   );     
+void LTC5556_decode_output(uint8_t output);
 #endif  // LTC5556_H
 
